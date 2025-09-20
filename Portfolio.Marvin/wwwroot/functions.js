431ae204b9ac2ef68cd7be8a752b83d87155563f
Marvin Drude <marvin@gidd.io>
2025-09-20 17:55:18 +0200

    Fixed js load

--- Portfolio.Marvin/wwwroot/functions.js
+++ Portfolio.Marvin/wwwroot/functions.js
@@ -5,3 +5,31 @@ window.scrollToBottom = (element, behavior) => {
         behavior: behavior || 'instant'
     });
 }
+
+window.loadScript = (url, sync) => {
+    
+    return new Promise((resolve, reject) => {
+       
+        const script = document.createElement('script');
+        script.type = 'text/javascript';
+        script.src = url;
+        script.async = !sync;
+        
+        script.onload = () => { resolve(true); };
+        script.onerror = () => { reject(false); };
+        
+        document.body.appendChild(script);
+        
+    });
+    
+}
+
+window.loadScripts = async (urls, sync) => {
+    
+    const promises = urls.map(url => {
+        return loadScript(url, sync);
+    });
+    
+    const results = await Promise.all(promises);
+    return results.filter(x => !x).length === 0;
+}
